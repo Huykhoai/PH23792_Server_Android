8224a919c5c2580fca3ae58fb4de764e8b92d4f5
huy <116376784+Huykhoai@users.noreply.github.com>
2023-04-12 22:53:06 +0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'secret': 'huy tdb ff ',
+  'database': 'mongodb+srv://huynqph23792:EOo0mM1pzUa8S5sY@cluster0.kgpv7xc.mongodb.net/CP17310?retryWrites=true&w=majority'
+  // ví dụ : mongodb+srv://huynvph20687:c1sas2Dnu6fPDo6F@mongo-huynvph20687.f01mcft.mongodb.net/test_mongodb?retryWrites=true&w=majority
+};
